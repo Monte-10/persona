b6054423960e5f01fd961c36c9cacbbb94949271
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-03-10 12:19:40 +0100

    Creo que ahora si

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
+npm install bootstrap@5.1.3
+npm install @popperjs/core@2.11.5
+
 python manage.py collectstatic --no-input
 
 python manage.py migrate
