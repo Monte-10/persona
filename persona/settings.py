e4aa8fb8a09f44efefadc2403bedb9c8533a54b7
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-03-13 21:33:07 +0100

    Pablo si estas leyendo los commits espero que al menos te rias un rato

--- persona/settings.py
+++ persona/settings.py
@@ -87,13 +87,11 @@ WSGI_APPLICATION = 'persona.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 import dj_database_url
-NEON_URL = 'postgres://Monte-10@ep-floral-flower-150155.eu-central-1.aws.neon.tech/persona'
-
-db_from_env = dj_database_url.config(default=NEON_URL, conn_max_age=500)
+#NEON_URL = 'postgres://Monte-10@ep-floral-flower-150155.eu-central-1.aws.neon.tech/persona'
+#db_from_env = dj_database_url.config(default=NEON_URL, conn_max_age=500)
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
-DATABASES['default'].update(db_from_env)
 
 
 
@@ -121,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
